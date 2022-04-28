72dda7ff67ad8fdef00fb2a3e2dcf700637d7834
Pedro Henrique <pedro.6571almeida@gmail.com>
2022-04-28 18:55:21 -0300

    Alterando método para salvar e alterar médico

--- src/main/java/com/clinicamedica/controllers/MedicoController.java
+++ src/main/java/com/clinicamedica/controllers/MedicoController.java
@@ -1,10 +1,12 @@
 package com.clinicamedica.controllers;
 
 import com.clinicamedica.entities.Medico;
+import com.clinicamedica.exceptions.FuncionarioException;
 import com.clinicamedica.services.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,7 +20,12 @@ public class MedicoController {
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void salvarMedico(@RequestBody Medico medico){
-        medicoService.salvarMedico(medico);
+        try{
+            medicoService.salvarMedico(medico);
+        }
+        catch (FuncionarioException exception){
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Email já cadastrado, tente outro.");
+        }
     }
 
     @GetMapping
@@ -31,5 +38,15 @@ public class MedicoController {
     @ResponseStatus(HttpStatus.OK)
     public List<Medico> listarMedicosPorIdEspecialidade(@RequestParam(required = false) Long idEspecialidade){
         return medicoService.listarMedicosPorIdEspecialidade(idEspecialidade);
+    }
+
+    @PutMapping(path = "/alterar-medico")
+    @ResponseStatus(HttpStatus.OK)
+    public void alterarMedico(@RequestBody Medico medico){
+        try {
+            medicoService.alterarMedico(medico);
+        } catch (FuncionarioException exception){
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Email já cadastrado, tente outro.");
+        }
     }
 }
--- src/main/java/com/clinicamedica/services/MedicoService.java
+++ src/main/java/com/clinicamedica/services/MedicoService.java
@@ -3,6 +3,7 @@ package com.clinicamedica.services;
 import com.clinicamedica.entities.Especialidade;
 import com.clinicamedica.entities.Funcionario;
 import com.clinicamedica.entities.Medico;
+import com.clinicamedica.exceptions.FuncionarioException;
 import com.clinicamedica.repositories.IMedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,12 +22,37 @@ public class MedicoService{
     private FuncionarioService funcionarioService;
 
     public void salvarMedico(Medico medico){
-        Optional<Funcionario> funcionario = funcionarioService.buscarFuncionarioPorEmail(medico.getEmail());
-        if(funcionario.isEmpty()) {
+        boolean emailJaExiste = funcionarioService.contarEmailsRepetidos(medico.getEmail()) > 0 ? true : false;
+        if(!emailJaExiste){
             if (medico.getEspecialidade().getIdEspecialidade() == null) {
                 especialidadeService.salvarEspecialidade(medico.getEspecialidade());
             }
             medicoRepository.save(medico);
+        } else {
+            System.err.println("Email já cadastrado no sistema, tente outro.");
+            throw new FuncionarioException("Email já cadastrado no sistema, tente outro.");
+        }
+    }
+
+    public void alterarMedico(Medico medicoAlterado){
+        Funcionario funcionario = funcionarioService.buscarFuncionarioPorId(medicoAlterado.getIdFuncionario()).get();
+        boolean emailFoiAlterado = !funcionario.getEmail().equals(medicoAlterado.getEmail());
+        if(emailFoiAlterado){
+            boolean emailJaExiste = funcionarioService.contarEmailsRepetidos(medicoAlterado.getEmail()) > 0 ? true : false;
+            if(emailJaExiste){
+                System.err.println("Email já cadastrado no sistema, tente outro.");
+                throw new FuncionarioException("Email já cadastrado no sistema, tente outro.");
+            } else {
+                if (medicoAlterado.getEspecialidade().getIdEspecialidade() == null) {
+                    especialidadeService.salvarEspecialidade(medicoAlterado.getEspecialidade());
+                }
+                medicoRepository.save(medicoAlterado);
+            }
+        } else {
+            if (medicoAlterado.getEspecialidade().getIdEspecialidade() == null) {
+                especialidadeService.salvarEspecialidade(medicoAlterado.getEspecialidade());
+            }
+            medicoRepository.save(medicoAlterado);
         }
     }
 
