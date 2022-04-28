0d47655298f77409a2f387839d7030e8facce629
Pedro Henrique <pedro.6571almeida@gmail.com>
2022-04-28 19:53:24 -0300

    Alterando método salvar

--- src/main/java/com/clinicamedica/services/EntradaSaidaMaterialService.java
+++ src/main/java/com/clinicamedica/services/EntradaSaidaMaterialService.java
@@ -23,8 +23,12 @@ public class EntradaSaidaMaterialService {
     private FuncionarioService funcionarioService;
 
     public EntradaSaidaMaterial salvarEntradaSaidaMaterial(EntradaSaidaMaterial entradaSaidaMaterial){
-        materialService.salvarMaterial(entradaSaidaMaterial.getMaterial());
-        funcionarioService.salvarFuncionario(entradaSaidaMaterial.getFuncionario());
+        if(entradaSaidaMaterial.getMaterial().getIdMaterial() == null){
+            materialService.salvarMaterial(entradaSaidaMaterial.getMaterial());
+        }
+        if(entradaSaidaMaterial.getFuncionario().getIdFuncionario() == null){
+            funcionarioService.salvarFuncionario(entradaSaidaMaterial.getFuncionario());
+        }
         return entradaSaidaMaterialRepository.save(entradaSaidaMaterial);
     }
 
