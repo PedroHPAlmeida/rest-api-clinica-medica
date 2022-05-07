62fd14a4c4686dd585e2b21ccf7f3f243c0efb93
Pedro Henrique <pedro.6571almeida@gmail.com>
2022-05-07 12:02:51 -0300

    ponto e vírgula

--- src/main/java/com/clinicamedica/controllers/RessarcimentoController.java
+++ src/main/java/com/clinicamedica/controllers/RessarcimentoController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/ressarcimentos")
